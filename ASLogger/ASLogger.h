51f6635f102abc08494a222eebd4b845056df4ad
quellish <github@quellish.neomailbox.com>
2014-08-02 20:07:02 -0700

    Initial Commit

---
+++ ASLogger/ASLogger.h
@@ -0,0 +1,13 @@
+//
+//  ASLogger.h
+//  ASLogger
+//
+//  Created by Dan Zinngrabe on 8/2/14.
+//  Copyright (c) 2014 Dan Zinngrabe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ASLogger : NSObject
+
+@end
---
+++ ASLogger/ASLogger.m
@@ -0,0 +1,13 @@
+//
+//  ASLogger.m
+//  ASLogger
+//
+//  Created by Dan Zinngrabe on 8/2/14.
+//  Copyright (c) 2014 Dan Zinngrabe. All rights reserved.
+//
+
+#import "ASLogger.h"
+
+@implementation ASLogger
+
+@end
